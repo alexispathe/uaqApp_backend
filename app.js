ece37bf09608028b1d9fba48a2f7a339baea0703
Alexis Pathe <alexispatheguillen@gmail.com>
2022-11-22 19:25:18 -0600

    Se manda el proyecto a produccion de react al backend

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 4011;
 const app = express();
 const cors = require('cors');
+const path =require('path');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+
 // rutas
 const hobbieRoutes = require('./src/router/hobbies-routes/hobbieRoute');
 const characterRoutes = require('./src/router/character-routes/characterRoute');
@@ -14,6 +16,10 @@ app.use(cors());
 app.listen(port,()=>{
     console.log(`Servidor conectado correctamente en el pureto ${port}`)
 });
+app.use('/', express.static('public',{redirect:false}));
 app.use('/api', hobbieRoutes);
 app.use('/api', characterRoutes);
+app.get('*',(req, res)=>{
+    res.sendFile(path.resolve('public/index.html'))
+})
 module.exports ={app};
